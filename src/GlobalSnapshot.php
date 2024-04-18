086d6375c2af191ea549505434040a7ddac3bcb4
Victor Westerlund <victor.vesterlund@gmail.com>
2024-04-18 13:23:30 +0200

    Initial code commit

---
+++ src/GlobalSnapshot.php
@@ -0,0 +1,49 @@
+<?php
+
+    namespace victorwesterlund;
+
+    // Capture the current state of all superglobals.
+    // This will save a copy of all keys and values and any changes made to the superglobals 
+    // can be restored to this point in time by calling $this->restore();
+    class GlobalSnapshot {
+        // Declare all PHP superglobals
+        private array $_ENV;
+        private array $_GET;
+        private array $_POST;
+        private array $_FILES;
+        private array $_SERVER;
+        private array $_COOKIE;
+        private array $_REQUEST;
+        private array $_SESSION;
+
+		// Declare additional PHP globals (for PHP 8.0+ compatability)
+		// These will not be captured, or altered by GlobalSnapshot
+        private int $argc;
+        private array $argv;
+        private ?array $__composer_autoload_files; // Native support for composer
+
+        public function __construct() {}
+
+        // Wipe all superglobals
+        private function truncate(string $global) {
+            global $$global;
+            $$global = [];
+        }
+
+        // Restore state of superglobals at the time of capture()
+        public function restore() {
+            foreach ($this as $global => $values) {
+                global $$global;
+
+                $this->truncate($global);
+                $$global = $this->{$global};
+            }
+        }
+
+		// Store current state of superglobals
+        public function capture() {
+            foreach (array_keys($GLOBALS) as $global) {
+                $this->{$global} = $GLOBALS[$global];
+            }
+        }
+    }
